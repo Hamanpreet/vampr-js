606c7b30b1613efa7f5d898145d17b517616f091
Hamanpreet <haman42626@gmail.com>
2023-07-10 20:23:09 -0400

    Use simple tree methods for features

--- vampire.js
+++ vampire.js
@@ -2,29 +2,62 @@ class Vampire {
   constructor(name, yearConverted) {
     this.name = name;
     this.yearConverted = yearConverted;
-    this.offspring = [];
     this.creator = null;
+    this.offspring = [];
   }
 
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator =  this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    let a = this.numberOfVampiresFromOriginal;
+    let b = vampire.numberOfVampiresFromOriginal;
+    if (a < b) {
+      return true;
+    }
+    return this.creator === vampire.creator;
+  }
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    for (const offspring of this.offspring) {
+      if (offspring !== name) {
+        return null;
+      }
+      return this;
+    }
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
 
   }
 
@@ -36,7 +69,7 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    
   }
 }
 
